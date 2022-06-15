75c9244b6a31cbf8acf30ee2d9f633d0f91f2e78
kamyshenJava <kamyshen@tutanota.com>
2022-06-15 15:29:58 +0300

    added models

---
+++ db/scripts/update_001.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+   id SERIAL PRIMARY KEY,
+   username VARCHAR NOT NULL,
+   email VARCHAR NOT NULL UNIQUE,
+   phone VARCHAR NOT NULL UNIQUE
+);
+
+CREATE TABLE sessions (
+  id SERIAL PRIMARY KEY,
+  name text
+);
+
+CREATE TABLE ticket (
+    id SERIAL PRIMARY KEY,
+    session_id INT NOT NULL REFERENCES sessions(id),
+    pos_row INT NOT NULL,
+    cell INT NOT NULL,
+    user_id INT NOT NULL REFERENCES users(id)
+);
